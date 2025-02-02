2e0f50270d22fde6a2d9da788fdb1a80631dc105
Devhoney2 <23f1002434@ds.study.iitm.ac.in>
2025-02-02 23:18:26 +0530

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+# Load the data
+data = pd.read_csv('')
+
+@app.get("/api")
+async def get_marks(name: list):
+    result = data[data['name'].isin(name)]['marks'].tolist()
+    return {"marks": result}
